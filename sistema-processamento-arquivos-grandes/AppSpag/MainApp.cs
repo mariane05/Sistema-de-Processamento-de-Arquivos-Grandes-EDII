1340442a4b5e24d2e1c02d2531d9c0f66ee03334
Victor Hugo <victor.030farias@gmail.com>
2025-11-24 12:44:45 -0300

    implementa debug de path para leitura do arquivo

--- sistema-processamento-arquivos-grandes/AppSpag/MainApp.cs
+++ sistema-processamento-arquivos-grandes/AppSpag/MainApp.cs
@@ -36,8 +36,23 @@ public class MainApp ()
     public static void validaParamsCompressao(string[] args)
     {
         //TODO: adicionar validações dos parâmetros <arquivo_original> && <arquivo_compactado> ao chamar o módulo
+        if (args[1] == null || args[1].Length == 0)
+        {
+            Console.WriteLine("Não foi encontrado o nome do arquivo para ser lido!");
+            return;
+        }
+
+        /*
+        IMPLEMENTAR DEPOIS QUE A COMPRESSÃO ESTIVER CERTINHO
+        if (args[2] == null || args[2].Length == 0)
+        {
+            Console.WriteLine("Não foi encontrado o nome que será atribuido ao arquivo compactado!");
+            return;
+        }
+        */
         
-        CompressaoApp.InitApp();
+
+        CompressaoApp.InitApp(args);
     }
 
     public static void validaParamsBuscaArquivoGrande(string[] args)
--- sistema-processamento-arquivos-grandes/Modules/Compressao/CompressaoApp.cs
+++ sistema-processamento-arquivos-grandes/Modules/Compressao/CompressaoApp.cs
@@ -2,17 +2,31 @@
 
 public class CompressaoApp
 {
-    public static void InitApp()
+    public static void InitApp(string[] args)
     {
-        testeLerArquivo();
+        debugaPathArquivo(args);
+        // return;
+
+        string caminhoArquivo = args[1]; //dado que args[0] é o nome do programa, args[1] vai ser o programa pra ler
+        testeLerArquivo(caminhoArquivo);
     }
     
 
-    public static void testeLerArquivo()
+    public static void testeLerArquivo(string caminhoArquivo)
     {
-        string caminhoArquivo = "../../livro01.txt";
+        
+        
         string primeiraLinhaLida = lerPrimeiraLinhaArquivo(caminhoArquivo);
         Console.WriteLine(primeiraLinhaLida);
+    }
+
+    public static void debugaPathArquivo(string[] args)
+    {
+        Console.WriteLine($"Path recebido: >{args[1]}<");
+
+        foreach (var b in System.Text.Encoding.UTF8.GetBytes(args[1]))
+            Console.Write($"{b:x2} ");
+        Console.WriteLine();
     }
 
     static string lerPrimeiraLinhaArquivo(string caminhoArquivo)
