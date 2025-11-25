c8a219f621bd868a070ad60edebc60a389aa0835
Victor Hugo <victor.030farias@gmail.com>
2025-11-24 21:47:17 -0300

    Cria algoritmos de construir o dicionário de frequências

--- sistema-processamento-arquivos-grandes/Modules/Compressao/CompressaoApp.cs
+++ sistema-processamento-arquivos-grandes/Modules/Compressao/CompressaoApp.cs
@@ -40,6 +40,37 @@ public class CompressaoApp
     } 
 
 
-    // public static Dictionary <char, long> construirTabelaDeFrequências
+
+
+
+
+    //todo:validar o quanto isso demora de fato
+    public static Dictionary <char, long> construirDicionarioDeFrequências(string pathToFile)
+    {
+        var frequencyDic = new Dictionary<char, long>();
+
+        using ( var reader = new StreamReader(pathToFile))
+        {
+            int caractereSendoLido;
+
+            while((caractereSendoLido = reader.Read()) != -1)
+            {
+                char c = (char)caractereSendoLido;
+                if (frequencyDic.ContainsKey(c))
+                    frequencyDic[c] ++;
+                else
+                    frequencyDic[c] = 1;
+                
+            }
+        }
+
+        return frequencyDic;
+    }
+
+    public static void lerDicionarioDeFrequências(Dictionary <char, long> dic)
+    {
+        foreach(var item in dic)
+            Console.WriteLine($"Caractere: '{item.Key}'; Ocorrências: ${item.Value}");
+    }
     
 }
