094284f3807bb5834cb234bc24d69c2f749b8668
Victor Farias <60015667+AHRGO@users.noreply.github.com>
2025-11-26 19:34:04 +0000

    Muda alguns comentários

--- sistema-processamento-arquivos-grandes/Modules/Compressao/CompressaoApp.cs
+++ sistema-processamento-arquivos-grandes/Modules/Compressao/CompressaoApp.cs
@@ -34,17 +34,17 @@ public class CompressaoApp
         using FileStream fsEntrada = new FileStream(caminhoEntrada, FileMode.Open, FileAccess.Read);
         long tamanhoOriginalTotalBytes = fsEntrada.Length;
 
-        // calcula número de blocos em bytes para saber quantos blocos teremos
+        // calcula número de blocos de 64k que serão gerados
         int numeroBlocos = (int)((tamanhoOriginalTotalBytes + TAMANHO_BLOCO - 1) / TAMANHO_BLOCO);
 
         using FileStream fsSaida = new FileStream(caminhoSaida, FileMode.Create, FileAccess.Write);
         using BinaryWriter writer = new BinaryWriter(fsSaida, Encoding.UTF8);
 
-        // 1) escreve cabeçalho geral (tamanho total + número de blocos)
+        // cabeçalho geral do arquivo
         writer.Write(tamanhoOriginalTotalBytes);
         writer.Write(numeroBlocos);
 
-        // 2) reserva espaço para o índice
+        //reserva espaço para os índices de cada bloco
         long posInicioIndice = fsSaida.Position;
         int tamanhoEntradaIndice = sizeof(long) + sizeof(int) + sizeof(long) + sizeof(int); // 8+4+8+4 = 24
         long tamanhoIndiceTotal = (long)numeroBlocos * tamanhoEntradaIndice;
